89483563442176426563fc561f0001bc13ed3891
axrylle <135582153+axrylle@users.noreply.github.com>
2024-11-12 15:22:24 +0800

    Update expiry-with-days.blade.php

--- resources/views/components/expiry-with-days.blade.php
+++ resources/views/components/expiry-with-days.blade.php
@@ -1,7 +1,7 @@
 <div>
     <span>{{ \Carbon\Carbon::parse($expiry)->format('M d, Y') }}</span>
     <br>
-    <span style="color: #4A5568; font-size: 0.85em;">
+    <span class="text-gray-800 dark:text-white" style="font-size: 0.85em;">
         {{ is_numeric($daysRemaining) ? round($daysRemaining) . ' day(s)' : $daysRemaining }}
     </span>
 </div>
