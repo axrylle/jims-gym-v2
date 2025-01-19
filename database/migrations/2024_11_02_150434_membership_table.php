dbce6c1bde54b52f628319658b4915cb37b7c20f
Kryx Loux <chthollyc@gmail.com>
2025-01-19 23:15:05 +0800

    Update 2024_11_02_150434_membership_table.php

--- database/migrations/2024_11_02_150434_membership_table.php
+++ database/migrations/2024_11_02_150434_membership_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('membership', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description');
+            $table->text('description');
             $table->integer('duration');  // Fixed column type
             $table->float('price');
             $table->timestamps();
