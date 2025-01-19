e02b4472678a011f473371b31edee7e394e64c77
Kryx Loux <chthollyc@gmail.com>
2025-01-19 20:44:51 +0800

    Update MembershipResource.php

--- app/Filament/Resources/MembershipResource.php
+++ app/Filament/Resources/MembershipResource.php
@@ -10,8 +10,11 @@ use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Filament\Tables\Columns\ImageColumn;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Section;
 
 class MembershipResource extends Resource
 {
@@ -22,21 +25,37 @@ class MembershipResource extends Resource
     public static function form(Form $form): Form
     {
         return $form
+            ->columns(3)
             ->schema([
-                Forms\Components\TextInput::make('name')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('description')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('duration')
-                    ->required()
-                    ->numeric()
-                    ->suffix('days'),
-                Forms\Components\TextInput::make('price')
-                    ->required()
-                    ->numeric()
-                    ->prefix('₱'),
+                Section::make('Membership Information')
+                    ->columns(2)
+                    ->columnSpan(2)
+                    ->schema([
+                        Forms\Components\TextInput::make('name')
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('duration')
+                            ->required()
+                            ->numeric()
+                            ->suffix('days'),
+                        Forms\Components\MarkdownEditor::make('description')
+                            ->required()
+                            ->maxLength(255)
+                            ->columnSpan('full'),
+                    ]),
+                Section::make('Membership Pricing')
+                    ->columnSpan(1)
+                    ->schema([
+                        FileUpload::make('image_name')
+                            ->disk('public')
+                            ->directory('images')
+                            ->image()
+                            ->imageEditor(),
+                        Forms\Components\TextInput::make('price')
+                            ->required()
+                            ->numeric()
+                            ->prefix('₱'),
+                    ]),
             ]);
     }
 
@@ -44,10 +63,14 @@ class MembershipResource extends Resource
     {
         return $table
             ->columns([
+                ImageColumn::make('image_name')
+                    ->disk('public')
+                    ->label('Image'),
                 Tables\Columns\TextColumn::make('name')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('description')
-                    ->searchable(),
+                    ->searchable()
+                    ->limit(25),
                 Tables\Columns\TextColumn::make('duration')
                     ->formatStateUsing(fn ($state) => $state . ' days')
                     ->numeric()
